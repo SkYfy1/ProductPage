1a7f017e9403619e5096f1365a42afe26d2817f3
SkYfy1 <festik.tawer@gmail.com>
2024-09-24 04:58:38 +0300

    slider

--- client/src/components/ImageSlider.jsx
+++ client/src/components/ImageSlider.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CollectionItem from './CollectionItem'
 
 const ImageSlider = ({ prod }) => {
     const [index, setIndex] = useState(0);
+    const [translate, setTranslate] = useState(0);
+    useEffect(() => {
+        console.log(translate)
+    }, [translate])
     return (
-        <div className='flex flex-col gap-6 overflow-hidden'>
-            <CollectionItem col={prod.collection} img={prod.images[0].image_url}/>
-            <div className='overflow-hidden w-[800px] flex gap-3 h-36'>
-                {/* placeholder */}
-                {prod.images.map(el => (
-                    <img src={el.image_url} key={el.image_url} alt="" className='object-cover rounded-md w-32' />
-                ))}
-                {/* <div className='object-fill rounded-md w-32 bg-gray-700'>f</div>
-                <div className='object-fill rounded-md w-32 bg-gray-700'>f</div>
-                <div className='object-fill rounded-md w-32 bg-gray-700'>f</div>
-                <div className='object-fill rounded-md w-32 bg-gray-700'>f</div> */}
-                {/* <img src="#" className='w-12 h-4 object-fill rounded-sm' alt="" />
-                <img src="#" className='w-12 h-4 object-fill rounded-sm' alt="" />
-                <img src="#" className='w-12 h-4 object-fill rounded-sm' alt="" />
-                <img src="#" className='w-12 h-4 object-fill rounded-sm' alt="" /> */}
+        <div className='flex flex-col w-[800px] gap-6 overflow-hidden'>
+            <CollectionItem col={prod.collection} img={prod.images[index].image_url} />
+            <div className='w-fit flex gap-3 h-36 relative'>
+                <svg onClick={() => setTranslate(prev => prev - 100)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 absolute top-1/2 text-white -translate-y-1/2 z-50 hover:cursor-help">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+                </svg>
+                <div className='flex gap-2 w-fit transition-transform duration-500 ease-out'
+                    style={{ transform: `translateX(-${translate}px)` }}
+                >
+                    {prod.images.map((el, ind) => (
+                        <img onClick={() => setIndex(ind)} src={el.image_url} key={ind} alt="" className={index == ind ? 'object-cover rounded-md w-32 border-2 border-blue-800' : 'object-cover rounded-md w-32 border-2 border-transparent hover:cursor-pointer'} />
+                    ))}
+                </div>
+                {/* {prod.images.map((el, ind) => (
+                        <img onClick={() => setIndex(ind)} src={el.image_url} key={ind} alt="" className={index == ind ? 'object-cover rounded-md w-32 border-2 border-blue-800' : 'object-cover rounded-md w-32 border-2 border-transparent'} />
+                    ))} */}
+                <svg onClick={() => translate != 500 && setTranslate(prev => prev + 100)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 top-1/2 -translate-y-1/2 -right-10 text-white -translate-x-[100%] absolute hover:cursor-help">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
             </div>
         </ div>
     )
